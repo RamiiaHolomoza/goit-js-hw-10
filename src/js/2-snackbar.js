b723ad19ae8744959804f72cc46ba36f583628ae
RamiiaHolomoza <162986900+RamiiaHolomoza@users.noreply.github.com>
2024-07-21 00:56:57 +0300

    add task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const delay = parseInt(event.target.delay.value);
+    console.log(delay);
+    const state = event.target.state.value;
+    console.log(state)
+    
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (state === 'fulfilled')
+            resolve(delay);
+        else {
+            reject(delay);
+        }
+    }, delay);
+});
+
+promise
+    .then((delay) => {
+        iziToast.success({
+            title: "Success",
+            message:  `✅ Fulfilled promise in ${delay}ms`
+            });
+        })
+    .catch((delay) => {
+        iziToast.error({
+            title: "Error",
+            message: `❌ Rejected promise in ${delay}ms`
+        });
+    })
+})
